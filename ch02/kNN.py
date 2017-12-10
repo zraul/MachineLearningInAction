085d40bf7feafaf56b83d806a1bd3e42a3af5bb7
zraul <zhengraul@gmail.com>
2017-12-10 22:09:43 +0800

    k邻近值识别手写数字例子

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -1,5 +1,8 @@
+# coding: utf8
+
+from numpy import tile, zeros, empty_like
 import operator
-from numpy import tile, zeros
+from os import listdir
 
 
 def classify0(inX, dataSet, labels, k):
@@ -30,3 +33,75 @@ def file2matrix(filename):
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
+
+
+def autoNorm(dataSet):
+    minValue = dataSet.min(0)
+    maxValue = dataSet.max(0)
+    ranges = maxValue - minValue
+    normDataSet = empty_like(dataSet, float)
+
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minValue, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minValue
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix("datingTestset2.txt")
+    normMat, range, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+
+    print "the tital error rate is: %f" % (errorCount/float(numTestVecs))
+
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+
+    return returnVect
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir("/Users/zhengwei/Downloads/machinelearninginaction/Ch02/trainingDigits/")
+
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('/Users/zhengwei/Downloads/machinelearninginaction/Ch02/trainingDigits/%s' % fileNameStr)
+
+    testFileList = listdir('/Users/zhengwei/Downloads/machinelearninginaction/Ch02/testDigits/')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('/Users/zhengwei/Downloads/machinelearninginaction/Ch02/testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        # print "the classifier came back with: %d, the real answer is %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+            print " %s the classifier came back with: %d, the real answer is %d" % (fileNameStr, classifierResult, classNumStr)
+
+    print "the total number of errors is:%d" % errorCount
+    print "the total error rate is: %f" % (errorCount/float(mTest))
+
+if __name__ == "__main__":
+    handwritingClassTest()
