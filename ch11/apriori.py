f0546c0853a9548f5be2a104e286688a2636b3c4
zraul <zhengraul@gmail.com>
2018-04-16 08:46:58 +0800

    commit

---
+++ ch11/apriori.py
@@ -0,0 +1,71 @@
+# -*- coding:utf-8 -*-
+
+from numpy import *
+
+def loadDataSet():
+    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+
+def createC1(dataSet):
+    C1 = []
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+
+    C1.sort()
+    # 要当成map的key，所以使用frozenset
+    return map(frozenset, C1)
+
+def scanD(D, Ck, minSupport):
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if not ssCnt.has_key(can):
+                    ssCnt[can] = 1
+                else:
+                    ssCnt[can] += 1
+
+    numIterms = float(len(D))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        support = ssCnt[key] / numIterms
+        if support >= minSupport:
+            retList.insert(0, key)
+        supportData[key] = support
+
+    return retList, supportData
+
+def aprioriGen(Lk, k):
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i + 1, lenLk):
+            L1 = list(Lk[i])[:k - 2]
+            L2 = list(Lk[j])[:k - 2]
+            if L1 == L2:
+                retList.append(Lk[i] | Lk[j])
+    return retList
+
+def apriori(dataSet, minSupport = 0.5):
+    C1 = createC1(dataSet)
+    D = map(set, dataSet)
+    L1, supportData = scanD(D, C1, minSupport)
+    L = [L1]
+    k = 2
+    while (len(L[k - 2]) > 0):
+        Ck = aprioriGen(L[k - 2] , k)
+        Lk, supK = scanD(D, Ck, minSupport)
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData
+
+if __name__ == '__main__':
+    dataSet = loadDataSet()
+    C1 = createC1(dataSet)
+    D = map(set, dataSet)
+    L1, suppData0 = scanD(D, C1, 0.5)
+    print '--------L1--------\n',L1
+    print '\n-----------------\n', suppData0
