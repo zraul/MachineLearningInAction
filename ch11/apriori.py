003a2678eddcc15406036a2232379d49e5b73dc6
zraul <zhengraul@gmail.com>
2018-05-17 18:03:55 +0800

    commit

--- ch11/apriori.py
+++ ch11/apriori.py
@@ -62,10 +62,45 @@ def apriori(dataSet, minSupport = 0.5):
         k += 1
     return L, supportData
 
+
+def generateRules(L, supportData, minConf=0.7):
+    bigRuleList = []
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            if (i > 1):
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
+    return bigRuleList
+
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []
+    for conseq in H:
+        conf = supportData[freqSet]/supportData[freqSet - conseq]
+        if conf >= minConf:
+            print freqSet - conseq,'-->','conf:',conf
+            brl.append((freqSet-conseq, conseq, conf))
+            prunedH.append(conseq)
+
+    return prunedH
+
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)):
+        Hmp1 = aprioriGen(H, m + 1)
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if (len(Hmp1) > 1):
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
+
+
 if __name__ == '__main__':
     dataSet = loadDataSet()
-    C1 = createC1(dataSet)
-    D = map(set, dataSet)
-    L1, suppData0 = scanD(D, C1, 0.5)
-    print '--------L1--------\n',L1
-    print '\n-----------------\n', suppData0
+    L, suppData = apriori(dataSet)
+    print L
+    print aprioriGen(L[0], 2)
+    # C1 = createC1(dataSet)
+    # D = map(set, dataSet)
+    # L1, suppData0 = scanD(D, C1, 0.5)
+    # print '--------L1--------\n',L1
+    # print '\n-----------------\n', suppData0
