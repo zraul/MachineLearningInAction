218e6dfee9e40443130f242cab1d3df5f9c96b8f
zraul <zhengraul@gmail.com>
2018-06-13 15:48:34 +0800

    手动做完所有Demo

--- ch14/svdRec.py
+++ ch14/svdRec.py
@@ -41,8 +41,10 @@ def cosSim(inA, inB):
     denom = la.norm(inA) * la.norm(inB)
     return 0.5 + 0.5 * (num / denom)
 
+# dataMat-数据矩阵
+# simMeas-相似度计算方法
 def standEst(dataMat, user, simMeas, item):
-    n = shape(dataMat)
+    n = shape(dataMat)[1]
     simTotal = 0.0
     ratSimTotal = 0.0
     for j in range(n):
@@ -55,7 +57,7 @@ def standEst(dataMat, user, simMeas, item):
         else:
             similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
 
-        print 'the %d and %d similarity is: %f' % (item, j, similarity)
+        # print 'the %d and %d similarity is: %f' % (item, j, similarity)
         simTotal += similarity
         ratSimTotal += similarity * userRating
 
@@ -66,7 +68,35 @@ def standEst(dataMat, user, simMeas, item):
 
 def recommend(dataMat, user, N = 3, simMeas = cosSim, estMethod=standEst):
     unratedItems = nonzero(dataMat[user, :].A == 0)[1]
+    if len(unratedItems) == 0:
+        return 'you rated everything'
+    itemScores = []
+    for item in unratedItems:
+        estimatedScore = estMethod(dataMat, user, simMeas, item)
+        itemScores.append((item, estimatedScore))
+
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N]
+
+def svdEst(dataMat, user, simMeas, item):
+    n = shape(dataMat)[1]
+    simTotal = 0.0
+    ratSimTotal = 0.0
+    U, Sigma, VT = la.svd(dataMat)
+    Sig4 = mat(eye(4) * Sigma[:4])
+    xformedItems = dataMat.T * U[:, :4] * Sig4.I
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0 or j == item:
+            continue
+        similarity = simMeas(xformedItems[item, :].T, xformedItems[j, :].T)
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+
+    if simTotal == 0:
+        return 0
+    else:
+        return ratSimTotal / simTotal
 
 if __name__ == '__main__':
-    Data = loadExData()
-    U, Sigma, VT = la.svd(Data)
+    myMat = mat(loadExData2())
+    print recommend(myMat, 1, estMethod=svdEst)
